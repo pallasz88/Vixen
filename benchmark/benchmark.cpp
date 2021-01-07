49de3a37f4e85b794da010a93ac4d3b2d8f0b3b7
Paal Laszlo <pallasz88@gmail.com>
2021-01-07 15:38:24 +0000

    Benchmark different GetEvalIndex implementations.

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,7 +5,7 @@
 #include "board.h"
 #include <iostream>
 
-constexpr int GetEvalIndex_(char c) noexcept
+constexpr int GetEvalIndexIfElse(char c) noexcept
 {
     if (c == 'p') return 0;
     else if (c == 'n') return 1;
@@ -18,7 +18,17 @@ constexpr int GetEvalIndex_(char c) noexcept
     else return -1;
 }
 
-int Evaluate(const Vixen::Board &board)
+constexpr auto GetEvalIndexLoop(char c) noexcept
+{
+    for (const auto &i : Vixen::evalMap)
+    {
+        if (i.first == c)
+            return i.second;
+    }
+    return -1;
+}
+
+int EvaluateIfElse(const Vixen::Board &board)
 {
     int score = board.GetMaterialBalance();
 
@@ -27,10 +37,26 @@ int Evaluate(const Vixen::Board &board)
     {
         if (piece == 'P' || piece == 'N' || piece == 'B' || piece == 'R' ||
             piece == 'p' || piece == 'n' || piece == 'b' || piece == 'r')
-            score += Vixen::Search::arrayLookUp[GetEvalIndex_(piece)][i];
+            score += Vixen::Search::arrayLookUp[GetEvalIndexIfElse(piece)][i];
         ++i;
     }
-    
+
+    return board.IsWhiteToMove() ? score : -score;
+}
+
+int EvaluateLoop(const Vixen::Board &board)
+{
+    int score = board.GetMaterialBalance();
+
+    unsigned i = 0;
+    for (const char piece : board.GetPieceList())
+    {
+        if (piece == 'P' || piece == 'N' || piece == 'B' || piece == 'R' ||
+            piece == 'p' || piece == 'n' || piece == 'b' || piece == 'r')
+            score += Vixen::Search::arrayLookUp[GetEvalIndexLoop(piece)][i];
+        ++i;
+    }
+
     return board.IsWhiteToMove() ? score : -score;
 }
 
@@ -39,17 +65,40 @@ static void BM_Evaluate(benchmark::State& state)
     Vixen::Board board;
     board.SetBoard(Vixen::Constants::TESTPOS1);
     for (auto _ : state)
-       Vixen::Search::Evaluate(board); 
+       benchmark::DoNotOptimize(Vixen::Search::Evaluate(board));
 }
 BENCHMARK(BM_Evaluate);
 
-static void BM_Eval2(benchmark::State& state)
+static void BM_EvalIfElse(benchmark::State& state)
 {
     Vixen::Board board;
     board.SetBoard(Vixen::Constants::TESTPOS1);
     for (auto _ : state)
-        Evaluate(board);
+        benchmark::DoNotOptimize(EvaluateIfElse(board));
 }
-BENCHMARK(BM_Eval2);
+BENCHMARK(BM_EvalIfElse);
+
+static void BM_EvalLoop(benchmark::State& state)
+{
+    Vixen::Board board;
+    board.SetBoard(Vixen::Constants::TESTPOS1);
+    for (auto _ : state)
+        benchmark::DoNotOptimize(EvaluateLoop(board));
+}
+BENCHMARK(BM_EvalLoop);
+
+static void BM_GetLoop(benchmark::State& state)
+{
+    for (auto _ : state)
+       benchmark::DoNotOptimize(GetEvalIndexLoop('r'));
+}
+BENCHMARK(BM_GetLoop);
+
+static void BM_GetIfElse(benchmark::State& state)
+{
+    for (auto _ : state)
+       benchmark::DoNotOptimize(GetEvalIndexIfElse('r'));
+}
+BENCHMARK(BM_GetIfElse);
 
 BENCHMARK_MAIN();
