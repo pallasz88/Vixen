22ea1d2fa95706fda8aaffd169e25a7dcc6eeed7
pallasz88 <pallasz88@gmail.com>
2023-12-06 21:25:01 +0000

    Deploying to gh-pages from @ pallasz88/Vixen@88fd3fb896f533307d92a75673e75220fe2a0e6b 🚀

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-mkdir build
-cd build
-
-cmake -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
-cmake --build . --target install -- -j %NUMBER_OF_PROCESSORS%
